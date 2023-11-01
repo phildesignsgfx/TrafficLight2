301c636525e699a659e317829822a44d4409e327
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-11-01 02:46:58 +0000

    done543

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,10 +14,11 @@ const Home = () => {
 	return (
 		<div className="traffic-light">
 		    <div onClick={() => setSelectedColor ("red")} className={"light red" + ((selectedColor === "red") ? "glow" : "")}></div>
-		    <div onClick={() => setSelectedColor ("yellow")}className={"light yellow" + ((selectedColor === "yellow") ? "glow" : "") }></div>
-		    <div onClick={() => setSelectedColor ("green")}className={"light green" + ((selectedColor === "green") ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("yellow")} className={"light yellow" + ((selectedColor === "yellow") ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("green")} className={"light green" + ((selectedColor === "green") ? "glow" : "")}></div>
 		</div>
 	);
 };
+
 
 export default Home;
