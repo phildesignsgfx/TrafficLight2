9be21f6abd212316c47f38b0b3a9a830d60c1f84
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-11-02 02:53:27 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,9 +15,9 @@ const Home = () => {
 		<div class="d-flex justify-content-center">
 			
 		<div className="traffic-light">
-		    <div onClick={() => setSelectedColor ("red")} className={"light red" + (selectedColor === "red" ? "glow" : "")}></div>
-		    <div onClick={() => setSelectedColor ("yellow")} className={"light yellow" + (selectedColor === "yellow" ? "glow" : "")}></div>
-		    <div onClick={() => setSelectedColor ("green")} className={"light green" + (selectedColor === "green" ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("red")} className={"light red " + (selectedColor === "red" ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("yellow")} className={"light yellow " + (selectedColor === "yellow" ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("green")} className={"light green " + (selectedColor === "green" ? "glow" : "")}></div>
 		</div>
 		</div>
 	);
