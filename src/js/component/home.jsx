70704a85b80000aeb078e788db78b9a870decce2
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-11-02 02:25:15 +0000

    completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-    const [ selectedColor, setSelectedColor ] = useState ("yellow");
+    const [ selectedColor, setSelectedColor ] = useState (" ");
 
 	
 
 
 	return (
+		<div class="d-flex justify-content-center">
+			
 		<div className="traffic-light">
-		    <div onClick={() => setSelectedColor ("red")} className={"light red" + ((selectedColor === "red") ? "glow" : "")}></div>
-		    <div onClick={() => setSelectedColor ("yellow")} className={"light yellow" + ((selectedColor === "yellow") ? "glow" : "")}></div>
-		    <div onClick={() => setSelectedColor ("green")} className={"light green" + ((selectedColor === "green") ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("red")} className={"light red" + (selectedColor === "red" ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("yellow")} className={"light yellow" + (selectedColor === "yellow" ? "glow" : "")}></div>
+		    <div onClick={() => setSelectedColor ("green")} className={"light green" + (selectedColor === "green" ? "glow" : "")}></div>
+		</div>
 		</div>
 	);
-};
+}
 
 
 export default Home;
